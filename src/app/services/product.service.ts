d1f3e583cd8e63d0b05ff07fc93efa7d179454c4
malleshgithub <malleshams.coign@gmail.com>
2022-01-04 21:22:17 +0530

    services

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+
+const baseUrl = 'http://localhost:7070/api/products';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Product> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByProductname(productname: any): Observable<Product[]> {
+    return this.http.get<Product[]>(`${baseUrl}?productname=${productname}`);
+  }
+}
