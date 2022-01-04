10020998c6f5593b681601d875999ba53ed55fa0
malleshgithub <malleshams.coign@gmail.com>
2022-01-04 21:41:43 +0530

    product list

---
+++ src/app/components/products-list/products-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent implements OnInit {
+  products?: Product[];
+  currentProduct: Product = {};
+  currentIndex = -1;
+  productname = '';
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.retrieveProducts();
+  }
+
+  retrieveProducts(): void {
+    this.productService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.products = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  refreshList(): void {
+    this.retrieveProducts();
+    this.currentProduct = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveProduct(product: Product, index: number): void {
+    this.currentProduct = product;
+    this.currentIndex = index;
+  }
+
+  removeAllProducts(): void {
+    this.productService.deleteAll()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.refreshList();
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  searchProductname(): void {
+    this.currentProduct = {};
+    this.currentIndex = -1;
+
+    this.productService.findByProductname(this.productname)
+      .subscribe({
+        next: (data) => {
+          this.products = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+
+}
